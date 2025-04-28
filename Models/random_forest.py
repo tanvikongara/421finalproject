111dca4d463228d90a5498e509f807daaa5ed0a3
Tanvi Kongara <tanvi.kongara@tamu.edu>
2025-04-28 14:51:37 -0500

    eda

--- Models/random_forest.py
+++ Models/random_forest.py
@@ -1,8 +1,10 @@
 import pandas as pd
 import os
-
+# pip install fastparquet
 # Get absolute path
 data_path = os.path.join(os.getcwd(), "Data", "total_merged_data.parquet")
 
 # Load parquet file
 df = pd.read_parquet(data_path)
+
+print(len(df))
